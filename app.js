e65a4863f32ceaed44153f9404585fd6789bd652
Rafa Calin <rafa.calin@yahoo.com>
2017-02-17 20:45:52 +0000

    add form

--- app.js
+++ app.js
@@ -29,12 +29,30 @@ app.get("/", function(req, res) {
     res.redirect("/blogs");
 });
 
+// index Route
 app.get("/blogs", function(req, res){
     Blog.find({}, function(err, blogs){
         if(err)
          console.log(err)
         else
           res.render("index", {blogs: blogs});
+    });
+});
+
+// new Route
+app.get("/blogs/new", function(req, res) {
+        res.render("new");
+});
+
+// Create Route
+app.post("/blogs", function(req, res){
+    //create blog
+    Blog.create(req.body.blog, function(err, newBlog){
+       if(err){
+        res.render("new");  
+       } else {
+        res.redirect("/blogs");        
+        }
     });
 });
 
